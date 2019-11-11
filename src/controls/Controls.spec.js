f74c3a8885ae97b55d61540d358f2981b278c34e
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 18:02:19 -0500

    test for open/close and lock/unlock buttons

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -5,8 +5,20 @@ import "@testing-library/jest-dom/extend-expect";
 
 import Controls from "./Controls";
 
-afterEach(cleanup);
-
 test("Controls render", () => {
 	render(<Controls />);
+});
+
+test("Has buttons for open/close and lock/unlock", () => {
+	const { getByText, rerender } = render(<Controls />);
+
+	const props = { closed: true, locked: true };
+
+	expect(getByText(/close gate/i)).toBeTruthy();
+	expect(getByText(/lock gate/i)).toBeTruthy();
+
+	rerender(<Controls {...props} />);
+
+	expect(getByText(/open gate/i)).toBeTruthy();
+	expect(getByText(/unlock gate/i)).toBeTruthy();
 });
