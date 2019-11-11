26a76970db2034b2345113a1313f9aada1ae329d
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 17:03:09 -0500

    check if dashboard renders display and controls, fixed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -9,6 +9,16 @@ test("Dashboard renders", () => {
 	render(<Dashboard />);
 });
 
+test("Dashboard renders display and controls", () => {
+	const wrap = render(<Dashboard />);
+
+	const display = wrap.container.querySelector(".display");
+	expect(display).toBeTruthy();
+
+	const controls = wrap.container.querySelector(".controls");
+	expect(controls).toBeTruthy();
+});
+
 test("Default state is unlocked and open", () => {
 	const { getByText } = render(<Dashboard />);
 
