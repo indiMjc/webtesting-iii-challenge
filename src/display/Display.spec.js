210a0688ab0fda9c457990ec074dabe8f9a9b8d1
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 17:20:25 -0500

    Display shows correct information based on state

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,24 @@
-// Test away!
+import React from "react";
+import { cleanup, render, rerender } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import Display from "./Display";
+
+test("Display renders", () => {
+	render(<Display />);
+});
+
+test("Displays if gate is open/closed and if it's locked/unlocked", () => {
+	const { getByText, rerender } = render(<Display />);
+
+	const props = { closed: true, locked: true };
+
+	const lockState = getByText(/unlocked/i);
+	const openState = getByText(/open/i);
+
+	rerender(<Display {...props} />);
+
+	expect(lockState.textContent).toBe("Locked");
+	expect(openState.textContent).toBe("Closed");
+});
