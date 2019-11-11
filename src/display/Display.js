2b0060efc7d192dfa2a948cabb5e1fa57f152756
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 18:15:45 -0500

    cleaned up unneeded imports, mvp met

--- src/display/Display.js
+++ src/display/Display.js
@@ -6,12 +6,8 @@ const Display = ({ closed, locked }) => {
 
 	return (
 		<div className="display panel">
-			<div data-testid="ifLocked" className={lockedClass}>
-				{locked ? "Locked" : "Unlocked"}
-			</div>
-			<div data-testid="ifClosed" className={closedClass}>
-				{closed ? "Closed" : "Open"}
-			</div>
+			<div className={lockedClass}>{locked ? "Locked" : "Unlocked"}</div>
+			<div className={closedClass}>{closed ? "Closed" : "Open"}</div>
 		</div>
 	);
 };
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, render, rerender } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 
