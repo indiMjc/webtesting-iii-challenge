e10a18d38e55609981bc1322a2d9b6d30c4f8687
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 17:50:51 -0500

    half of the led test finished

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -17,8 +17,31 @@ test("Displays if gate is open/closed and if it's locked/unlocked", () => {
 	const lockState = getByText(/unlocked/i);
 	const openState = getByText(/open/i);
 
+	expect(lockState.textContent).toBe("Unlocked");
+	expect(openState.textContent).toBe("Open");
+
 	rerender(<Display {...props} />);
 
 	expect(lockState.textContent).toBe("Locked");
 	expect(openState.textContent).toBe("Closed");
+});
+
+test("When gate is locked or closed, led is red.  When gate is unlocked or open, led is gree,", () => {
+	const { rerender } = render(<Display />);
+
+	const props = { closed: true, locked: true };
+
+	const red = document.querySelector(".red-led");
+	const green = document.querySelector(".green-led");
+
+	expect(red).toBeFalsy();
+	expect(green).toBeTruthy();
+
+	// rerender(<Display {...props} />);
+
+	// const redLed = document.querySelector(".red-led");
+	// const greenLed = document.querySelector(".green-led");
+
+	// expect(red).toBeTruthy();
+	// expect(green).toBeFalsy();
 });
