24f5ee19c6762441c7a499227cfbd915b6b061ad
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-11 16:43:16 -0500

    test that Dashboard component renders and default state is "unlocked" and "open"

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from "react";
+import { cleanup, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+afterEach(cleanup);
+
+test("Controls render", () => {
+	render(<Controls />);
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
-// Test away
+import React from "react";
+import { cleanup, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard renders", () => {
+	render(<Dashboard />);
+});
+
+test("Default state is unlocked and open", () => {
+	const { getByText } = render(<Dashboard />);
+
+	getByText(/unlocked/i);
+	getByText(/open/i);
+});
